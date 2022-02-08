087db11e99f299f617bdbfa28ed6f257a138ee2c
Dr-Insanity <cichonquincy@gmail.com>
2022-02-08 14:59:53 +0100

    Added some colors.

---
+++ termcolors.py
@@ -0,0 +1,16 @@
+class Colors:
+    """ansi ESC (escape) codes"""
+    grey = 0x2f3136
+    HEADER = '\033[95m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    yellow = '\033[38;5;220m'
+    red = '\033[91m'
+    ENDC = '\033[0m'
+    bold = '\033[1m'
+    unbold = '\033[22m'
+    UNDERLINE = '\033[4m'
+    gold = '\033[38;5;208m'
+    white = '\033[38;5;15m'
+    blurple = '\033[38;5;117m'
